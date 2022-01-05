6490ba63d4c383ff3dab793c016266b53e258107
Maria Eduarda Grocholski <megrocholski@gmail.com>
2022-01-05 18:23:40 -0300

    Login e create account

--- src/hooks/Auth/index.tsx
+++ src/hooks/Auth/index.tsx
@@ -27,11 +27,32 @@ interface AuthContextData {
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
-const [user, setUser] = useState<User>({} as User);
+const [user, setUser] = useState<User>(() => {
+  const storagedUser = localStorage.getItem('@krud:user');
+  const storagedToken = localStorage.getItem('@krud:token');
 
-const signIn = useCallback(async () => {}, []);
+  if (storagedToken && storagedUser) {
+    api.defaults.headers.common['authorization'] = `Bearer ${storagedToken}`;
+    return JSON.parse(storagedUser);
+  }
+  return {} as User;
+});
 
-const signUp = useCallback(async () => {}, []);
+const signIn = useCallback(async (credentials: AuthCredentials) => {
+  const { data }: { data: Response<LoginResponse> } = await api.post('auth/login', credentials);
+
+  localStorage.setItem('@krud:user', JSON.stringify(data.content?.user));
+  localStorage.setItem('@krud:token', data.content?.token as string);
+
+  api.defaults.headers.common['authorization'] = `Bearer ${data.content?.token}`;
+
+  setUser(data.content?.user as User);
+}, []);
+
+const signUp = useCallback(async (userData: SignUpData): Promise<Response<User>> => {
+  const { data }: { data: Response<User> } = await api.post('user/create', userData);
+  return data;
+}, []);
 
 export const AuthProvider: React.FC = ({ children }) => {
   return <AuthContext.Provider value={{ user, signIn, signUp }}>{children}</AuthContext.Provider>;
